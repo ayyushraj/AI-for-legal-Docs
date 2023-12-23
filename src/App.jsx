3de2789f162140ad651a69d0cb193ab3915e03d3
Tanmay Deshpande <dsp.tanmay@gmail.com>
2023-12-23 10:13:49 +0530

    added basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
-import OcrComponent from "./pages/ocrComponent"
+import React from 'react';
+import { ChakraProvider, CSSReset, Box, Button, Link as ChakraLink, VStack } from '@chakra-ui/react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+// import './App.css';
+
+// import LegalTemplates from './pages/LegalTemplates';
+import OcrSummarizer from './pages/ocrComponent';
+// import LegalChatbot from './pages/LegalChatbot';
 
 const App = () => {
   return (
-    <>
-      <OcrComponent />
-    </>
+    <ChakraProvider>
+      <Router>
+        <CSSReset />
+        <Box className="App" minH="100vh" display="flex" justifyContent="center" alignItems="center">
+          <VStack spacing={4}>
+            <Link to="/legal-templates">
+              <ChakraLink>
+                <Button colorScheme="blue" size="lg">
+                  Legal Templates
+                </Button>
+              </ChakraLink>
+            </Link>
+            <Link to="/ocr-summarizer">
+              <ChakraLink>
+                <Button colorScheme="blue" size="lg">
+                  OCR Summarizer
+                </Button>
+              </ChakraLink>
+            </Link>
+            <Link to="/legal-chatbot">
+              <ChakraLink>
+                <Button colorScheme="blue" size="lg">
+                  Legal Chatbot
+                </Button>
+              </ChakraLink>
+            </Link>
+          </VStack>
+          <Routes>
+            <Route path="/ocr-summarizer" element={<OcrSummarizer/>} />
+          </Routes>
+        </Box>
+      </Router>
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
